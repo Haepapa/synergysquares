fc1bc228eb4e1c6bf65d4c5ac02792e8f23eabad
timotewb <timote.wb@gmail.com>
2025-05-22 08:11:36 +1200

    check for existing

--- app/appwrite_utils.go
+++ app/appwrite_utils.go
@@ -1,8 +1,12 @@
 package app
 
 import (
+	"fmt"
+
 	"github.com/appwrite/sdk-for-go/appwrite"
 	"github.com/appwrite/sdk-for-go/databases"
+	"github.com/appwrite/sdk-for-go/id"
+	"github.com/appwrite/sdk-for-go/models"
 )
 
 // Export vrs
@@ -18,4 +22,49 @@ func Utils(){
 		appwrite.WithKey(AppwriteRESDEFAPIKey),
 	)
 	AppwriteDatabase = appwrite.NewDatabases(client)
+}
+
+func CreateDatabase(name string) (*models.Database, error) {
+	// List all databases
+	databases, err := AppwriteDatabase.List()
+	if err != nil {
+		fmt.Println("Error listing databases:", err)
+		return nil, err
+	}
+	for _, db := range databases.Databases {
+		if db.Name == name {
+			fmt.Println("Database already exists with id:", db.Id)
+			return &db, nil
+		}
+	}
+	// Create a database
+	db, err := AppwriteDatabase.Create(id.Unique(), name)
+	if err != nil {
+		fmt.Println("Error creating database:", err)
+		return nil, err
+	}
+	fmt.Println("Database created with id:", db.Id)
+	return db, nil
+}
+func CreateCollection(dbId string, name string) (*models.Collection, error) {
+	// List all collections
+	collections, err := AppwriteDatabase.ListCollections(dbId)
+	if err != nil {
+		fmt.Println("Error listing collections:", err)
+		return nil, err
+	}
+	for _, col := range collections.Collections {
+		if col.Name == name {
+			fmt.Println("Collection already exists with id:", col.Id)
+			return &col, nil
+		}
+	}
+	// Create a collection
+	col, err := AppwriteDatabase.CreateCollection(dbId, id.Unique(), name)
+	if err != nil {
+		fmt.Println("Error creating collection:", err)
+		return nil, err
+	}
+	fmt.Println("Collection created with id:", col.Id)
+	return col, nil
 }
--- main.go
+++ main.go
@@ -12,13 +12,35 @@ import (
 
 
 func main() {
+	// supress insecure warning
 	http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
+
+	// Initialize Appwrite client
 	app.Utils()
 
-	// Create a database
-	_, err := app.AppwriteDatabase.Create(id.Unique(), "synergysquares")
+	// List all databases
+	databases, err := app.AppwriteDatabase.List()
 	if err != nil {
-		fmt.Println("Error creating document:", err)
+		fmt.Println("Error listing databases:", err)
 		return
 	}
+	fmt.Println("Databases:")
+	for _, db := range databases.Databases {
+		fmt.Printf("ID: %s, Name: %s\n", db.Id, db.Name)
+	}
+	// Create a database
+	db, err := app.AppwriteDatabase.Create(id.Unique(), "synergysquares1")
+	if err != nil {
+		fmt.Println("Error creating database:", err)
+		return
+	}
+	fmt.Println("Database created with id:", db.Id)
+
+	// Create a collection(s)
+	colContactUs, err := app.AppwriteDatabase.CreateCollection(db.Id, id.Unique(), "contactus")
+	if err != nil {
+		fmt.Println("Error creating collection:", err)
+		return
+	}
+	fmt.Println("Collection created with id:", colContactUs.Id)
 }
